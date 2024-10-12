27dfda23efa024bb47203a69033397aef5056b33
Gurban Gurbanov <ggurbanov@Gurbans-MacBook-Pro.local>
2024-10-13 00:08:41 +0400

    Updates

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/BumbuKhan/puppy
 
 go 1.23.2
+
+require github.com/BumbuKhan/dog v0.0.0-20241012200154-f877cd1495b8 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/BumbuKhan/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBar() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Barks())
 }
